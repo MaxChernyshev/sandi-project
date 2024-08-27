df551a46da6a343ee847c0b483f8075e049e2cc4
MaxChernyshev <maksim.chernyshev@gmail.com>
2024-08-26 20:33:32 -0700

    small fix

--- resources/views/admin/statement/create.blade.php
+++ resources/views/admin/statement/create.blade.php
@@ -28,7 +28,7 @@
                                 @enderror
                             </div>
 
-                            <x-admin.quill-editor title="Statement Content" name="content" id="hidden-content"/>
+                            <x-admin.quill-editor title="Statement Content" name="content" id="hidden-content" value=""/>
                             <div class="flex flex-col gap-5.5 p-6.5 pt-0">
                                 @error('content')
                                 <div class="error text-frontred">{{ $message }}</div>
