4347b5e86dec4610ba59b314a0fffba875726196
MaxChernyshev <maksim.chernyshev@gmail.com>
2025-02-03 00:11:12 -0800

    product show blade fixed

--- resources/views/front/product/show.blade.php
+++ resources/views/front/product/show.blade.php
@@ -4,27 +4,30 @@
     <!-- Feature Section -->
     <section id="statement">
         <!-- Feature Container -->
-        <div class="relative container flex flex-row max-w-6xl mx-auto mt-16 px-6 text-gray-900 md:flex-row md:px-0 text-4xl ql-editor">
+        <div class="relative container flex flex-row max-w-6xl mx-auto mt-16 px-6 text-gray-900 md:flex-row md:px-0 text-4xl">
             <h1>{!! $product->name !!}</h1>
         </div>
 
-        <div class="relative container flex flex-row max-w-6xl mx-auto mt-16 px-6 text-gray-900 md:flex-row md:px-0 text-4xl ql-editor">
+        <div class="relative container flex flex-row max-w-6xl mx-auto mt-16 px-6 text-gray-900 md:flex-row md:px-0 text-4xl">
             @if(isset($product->image->image_path))
                 <img src="{{URL::asset($product->image->image_path)}}" alt="{!! $product->name !!}" class="h-30">
             @endif
         </div>
 
-        <div class="relative container max-w-6xl mx-auto mb-8 px-6 text-gray-900 md:flex-row md:px-0 ql-editor">
+        <div class="relative container max-w-6xl mx-auto mb-8 px-6 text-gray-900 md:flex-row md:px-0">
             <p>{!! $product->description !!}</p>
         </div>
 
-        <div class="relative container max-w-6xl mx-auto mb-8 px-6 text-gray-900 md:flex-row md:px-0 ql-editor">
+        <div class="relative container max-w-6xl mx-auto mb-8 px-6 text-gray-900 md:flex-row md:px-0">
             <p>$ {!! $product->price !!}</p>
         </div>
 
         @if(isset($product->instruction))
-            <div class="relative container max-w-6xl mx-auto mb-8 px-6 text-gray-900 md:flex-row md:px-0 ql-editor">
-                <p>{{ $product->instruction->content }}</p>
+            <div class="relative container max-w-6xl mx-auto mb-8 px-6 text-gray-900 md:flex-row md:px-0 text-2xl">
+            <h1>How to cook</h1>
+            </div>
+            <div class="relative container max-w-6xl mx-auto mb-8 px-6 text-gray-900 md:flex-row md:px-0">
+                <p>{!! $product->instruction->content !!}</p>
             </div>
         @endif
     </section>
